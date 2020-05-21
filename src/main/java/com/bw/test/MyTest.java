e6afdf186a766ecba7e1b4e4449b43ce5eca6b91
suxiangyu <32907@E-xiaomishouji.mshome.net>
2020-05-21 19:26:00 +0800

    第一次提交

---
+++ src/main/java/com/bw/test/MyTest.java
@@ -0,0 +1,14 @@
+package com.bw.test;
+/**
+ * 
+	*说明:
+    * @ClassName: MyTest
+    * @Description: TODO(测试)
+    * @author suxiangyu(email:3290785479@qq.com)
+    * @date 2020年5月21日下午7:24:38
+ */
+public class MyTest {
+	public static void main(String[] args) {
+		System.out.println("苏祥宇");
+	}
+}	
